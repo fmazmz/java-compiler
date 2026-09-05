8be813cd0c1938c1b3af75049e5673d20280638d
WHITEROSE <firasmoussa60@gmail.com>
2026-09-05 11:52:26 +0200

    make a generic tokentype first

--- src/TokenType.java
+++ src/TokenType.java
@@ -1,40 +1,10 @@
 public enum TokenType {
-    // Expression symbols
-    LPAREN("("),
-    RPAREN(")"),
-    LBRACE("{"),
-    RBRACE("}"),
-    SEMI(";"),
-    COMMA(","),
-    DOT("."),
-    PLUS("+"),
-    MINUS("-"),
-    STAR("*"),
-    PERCENT("%"),
-    BANG("!"),
-    ASSIGN("="),
-    LT("<"),
-    GT(">"),
-
-    // Keywords
-    CLASS("class"),
-    PUBLIC("public"),
-    PRIVATE("private"),
-    STATIC("static"),
-    VOID("void"),
-    INT("int"),
-    BOOLEAN("boolean"),
-    IF("if"),
-    ELSE("else"),
-    WHILE("while"),
-    RETURN("return"),
-    TRUE("true"),
-    FALSE("false"),
-    EOF("");
-
-    private String token;
-
-    public TokenType tokenType(String token) {
-        this.token(token);
-    }
+    KEYWORD,
+    IDENTIFIER,
+    LITERAL,
+    OPERATOR,
+    PUNCTIOATION,
+    WHITESPACE,
+    COMMENT,
+    UNKNOWN
 }
