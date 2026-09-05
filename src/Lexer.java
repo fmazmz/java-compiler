1f1d244faf1c79a0ee726eae17978cbeab89d070
WHITEROSE <firasmoussa60@gmail.com>
2026-09-05 11:54:24 +0200

    lexer skeleton

---
+++ src/Lexer.java
@@ -0,0 +1,11 @@
+import java.awt.List;
+
+public class Lexer {
+    private String input;
+    private int currentPos;
+
+    public Lexer(String input) {
+        this.input = input;
+        this.currentPos = 0;
+    }
+}
