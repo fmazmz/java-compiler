c3687343cbbcfa7e49fb8d18562bfe0279121543
WHITEROSE <firasmoussa60@gmail.com>
2026-09-05 19:20:27 +0200

    write out the xplicit tokentypes that will be used in the parser

--- src/TokenType.java
+++ src/TokenType.java
@@ -1,10 +1,54 @@
 public enum TokenType {
-    KEYWORD,
-    IDENTIFIER,
-    LITERAL,
-    OPERATOR,
-    PUNCTIOATION,
-    WHITESPACE,
-    COMMENT,
-    UNKNOWN
+    LPAREN("("),
+    RPAREN(")"),
+    LBRACE("{"),
+    RBRACE("}"),
+    SEMI(";"),
+    COMMA(","),
+    DOT("."),
+
+    PLUS("+"),
+    MINUS("-"),
+    STAR("*"),
+    SLASH("/"),
+    PERCENT("%"),
+    BANG("!"),
+    ASSIGN("="),
+    LT("<"),
+    GT(">"),
+
+    EQ("=="),
+    NEQ("!="),
+    LTE("<="),
+    GTE(">="),
+    AND("&&"),
+    OR("||"),
+
+    IDENT(""),
+    INT_LIT(""),
+
+    CLASS("class"),
+    PUBLIC("public"),
+    STATIC("static"),
+    VOID("void"),
+    INT("int"),
+    BOOLEAN("boolean"),
+    IF("if"),
+    ELSE("else"),
+    WHILE("while"),
+    RETURN("return"),
+    TRUE("true"),
+    FALSE("false"),
+
+    EOF("");
+
+    private final String lexeme;
+
+    TokenType(String lexeme) {
+        this.lexeme = lexeme;
+    }
+
+    public String getLexeme() {
+        return lexeme;
+    }
 }
