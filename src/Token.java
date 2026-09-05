45fd5561ed8e324907b24f80d1cc25f3e3126a06
WHITEROSE <firasmoussa60@gmail.com>
2026-09-05 11:57:22 +0200

    add Token

---
+++ src/Token.java
@@ -0,0 +1,22 @@
+public class Token {
+    private TokenType type;
+    private String val;
+
+    public Token(TokenType type, String val) {
+        this.type = type;
+        this.val = val;
+    }
+
+    public TokenType getType() {
+        return type;
+    }
+
+    public String getValue() {
+        return val;
+    }
+
+    @Override
+    public String toString() {
+        return "Token{" + "type=" + type + ", value ='" + val + '\'' + '}';
+    }
+}
